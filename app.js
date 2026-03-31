45bbf9fc8cbcdb9349ec59639b19b8dcd65b641c
AKSH123 <akshath1718@gmail.com>
2026-03-31 17:40:54 +0530

    Initial commit

--- app.js
+++ app.js
@@ -154,6 +154,13 @@ app.use((err, req, res, next) => {
 });
 
 
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+
+
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 });
